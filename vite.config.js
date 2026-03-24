293ec263c77fde5f637f0de6016603a2667ac002
Sacha Schlumpf <sachaschlumpf@gmail.com>
2026-03-24 09:42:50 +0100

    Deploy website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://sacha-schlumpf.github.io/006-pokemon/",
   plugins: [react()],
-})
+});
